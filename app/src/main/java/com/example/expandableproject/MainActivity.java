cc2ad72a7b91413f76d4ff9ec34e14e4c3107222
Brandon Carranza <brandon.carranza@janaq.com>
2020-02-12 00:15:56 -0500

    finished

--- app/src/main/java/com/example/expandableproject/MainActivity.java
+++ app/src/main/java/com/example/expandableproject/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.expandableproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<NotasBimestre> bimestres = new ArrayList<>();
+
+        ArrayList<Nota> notas = new ArrayList<>();
+        notas.add(new Nota("14"));
+        notas.add(new Nota("11"));
+        notas.add(new Nota("13"));
+        notas.add(new Nota("12"));
+
+        NotasBimestre notasBimestre = new NotasBimestre("Bimestre I",notas);
+        bimestres.add(notasBimestre);
+
+        ArrayList<Nota> notas2 = new ArrayList<>();
+        notas2.add(new Nota("11"));
+        notas2.add(new Nota("19"));
+        notas2.add(new Nota("20"));
+        notas2.add(new Nota("19"));
+
+        NotasBimestre notas2Bimestre = new NotasBimestre("Bimestre II",notas2);
+        bimestres.add(notas2Bimestre);
+
+        NotaAdapter notaAdapter = new NotaAdapter(bimestres);
+        recyclerView.setAdapter(notaAdapter);
     }
 }
--- app/src/main/java/com/example/expandableproject/NotasBimestreViewHolder.java
+++ app/src/main/java/com/example/expandableproject/NotasBimestreViewHolder.java
@@ -1,20 +1,51 @@
 package com.example.expandableproject;
 
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder;
 
 public class NotasBimestreViewHolder extends GroupViewHolder {
 
+    private ImageView imageView;
     private TextView textView;
 
     public NotasBimestreViewHolder(View itemView) {
         super(itemView);
+        imageView = itemView.findViewById(R.id.iv_arrow);
         textView = itemView.findViewById(R.id.tv_bimestre);
     }
 
     public void bind(NotasBimestre notasBimestre){
         textView.setText(notasBimestre.getTitle());
+    }
+
+    @Override
+    public void expand() {
+        animateExpand();
+    }
+
+    @Override
+    public void collapse() {
+        animateCollapse();
+    }
+
+    private void animateExpand(){
+        RotateAnimation rotate = new RotateAnimation
+                (360,180, Animation.RELATIVE_TO_SELF, 0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotate.setDuration(300);
+        rotate.setFillAfter(true);
+        imageView.setAnimation(rotate);
+    }
+
+    private void animateCollapse(){
+        RotateAnimation rotate = new RotateAnimation
+                (180,360, Animation.RELATIVE_TO_SELF, 0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotate.setDuration(300);
+        rotate.setFillAfter(true);
+        imageView.setAnimation(rotate);
     }
 }
